47c8b90aa156a3af63ea1725f2654aa5f3070cd2
Lavkush Maurya <lavkushmaurya147@gmail.com>
2023-06-28 09:33:31 +0530

    direct db

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,36 +1,37 @@
 import mongoose from "mongoose";
 
-const DB_URL = process.env.DB_URL;
+const DB_URL =
+  "mongodb+srv://admin:admin@cluster0.rfxyxuu.mongodb.net/?retryWrites=true&w=majority";
 
 if (!DB_URL) {
-	throw new Error(
-		"Please define the DB_URL environment variable inside .env.local"
-	);
+  throw new Error(
+    "Please define the DB_URL environment variable inside .env.local"
+  );
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-	cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 const dbConnect = async () => {
-	if (cached.conn) {
-		return cached.conn;
-	}
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-	if (!cached.promise) {
-		const options = {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		};
+  if (!cached.promise) {
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
 
-		cached.promise = mongoose.connect(DB_URL, options).then((mongoose) => {
-			return mongoose;
-		});
-	}
-	cached.conn = await cached.promise;
-	return cached.conn;
+    cached.promise = mongoose.connect(DB_URL, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default dbConnect;
